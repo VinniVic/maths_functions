4017fce788cca0a4b60a434c7e48250e989a2071
Vinni Victor <P15232864@GWHXX651WIN005W.lec-admin.dmu.ac.uk>
2017-01-17 13:39:09 +0000

    introduced a third line

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int main()
 {
-    printf ("Hello World!\n");
-    return 0;
-}
+	printf("Hello World!!\n");
+	printf("fare thee well!!\n") ; 
+
+	return 0; 
+} 
